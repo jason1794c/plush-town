ceb07dffe9276476eb5960f45f0e7bf6a4381646
jason1794c <69557789+jason1794c@users.noreply.github.com>
2020-11-20 17:43:23 -0500

    Created server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+})
+
+app.get('/', (req, res) => {
+    res.send('Server is ready.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
